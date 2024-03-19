0693e26d4376c7be855dc5d443511495aa717095
Fabio Andre <fabio.mojica@ucb.edu.bo>
2024-03-18 23:16:12 -0400

    Refactorizacion en la clase juego.js

--- src/Juego.js
+++ src/Juego.js
@@ -1,34 +1,51 @@
 class Juego {
     constructor() {
-      this.rolls = new Array(21).fill(0); // Inicializa un array de 21 elementos con 0
+      this.rolls = new Array(21).fill(0);
       this.currentRoll = 0;
     }
   
     roll(pins) {
       this.rolls[this.currentRoll++] = pins;
     }
-
+  
     score() {
-        let score = 0;
-        let frameIndex = 0;
-        for (let frame = 0; frame < 10; frame++) {
-          if (this.rolls[frameIndex] === 10) { // strike
-            score += 10 + this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
-            frameIndex++;
-          } else if (this.isSpare(frameIndex)) {
-            score += 10 + this.rolls[frameIndex + 2];
-            frameIndex += 2;
-          } else {
-            score += this.rolls[frameIndex] + this.rolls[frameIndex + 1];
-            frameIndex += 2;
-          }
+      let score = 0;
+      let frameIndex = 0;
+      for (let frame = 0; frame < 10; frame++) {
+        if (this.isStrike(frameIndex)) {
+          score += 10 + this.strikeBonus(frameIndex);
+          frameIndex++;
+        } else if (this.isSpare(frameIndex)) {
+          score += 10 + this.spareBonus(frameIndex);
+          frameIndex += 2;
+        } else {
+          score += this.sumOfBallsInFrame(frameIndex);
+          frameIndex += 2;
         }
-        return score;
       }
-    
+      return score;
+    }
+  
+    sumOfBallsInFrame(frameIndex) {
+      return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+    }
+  
+    spareBonus(frameIndex) {
+      return this.rolls[frameIndex + 2];
+    }
+  
+    strikeBonus(frameIndex) {
+      return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+    }
+  
     isSpare(frameIndex) {
       return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10;
     }
+  
+    isStrike(frameIndex) {
+      return this.rolls[frameIndex] === 10;
+    }
   }
   
-export default Juego;
+  export default Juego;
+  
